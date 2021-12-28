e0bf8a1ca2f15aaa882f8e7f603c98a7f7a37ebc
Ryan Bender <ryan_bender@ymail.com>
2021-12-28 17:09:16 -0600

    phase2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import harvardArt from './data/harvardArt';
 import GalleryNavigation from './components/GalleryNavigation';
+import GalleryView from './components/GalleryView';
+import {Route, useParams} from 'react-dom-router'
+
 
 const galleries = harvardArt.records;
 
 function App() {
+  const {id} = useParams();
   return (
     <div>
       <GalleryNavigation galleries={harvardArt.records} />
+      <Route>
+          <GalleryView galleries={harvardArt.records}></GalleryView>
+      </Route>
     </div>
   );
 }
---
+++ src/components/GalleryView/index.js
@@ -0,0 +1,7 @@
+function Galleryview({galleries}) {
+    return (
+        <h1>GalleryView</h1>
+        );
+}
+
+export default Galleryview;
